db1111f4f28522ae8f2467931adab9df1899e1e8
Phuong Dong <phuongdong271299@gmail.com>
2022-01-18 23:25:38 +0700

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           Lê Ngọc Phương Đông
         </p>
         <a
           className="App-link"
